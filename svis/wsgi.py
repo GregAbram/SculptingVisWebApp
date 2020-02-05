d80c4d21429e6ec3cf86edafa265f8334018486b
root <root@sculptingvis.tacc.utexas.edu>
2020-02-05 09:27:30 -0600

    deletion

--- svis/wsgi.py
+++ svis/wsgi.py
@@ -9,9 +9,17 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import sys
 print("WSGI PYTHON VERSION", sys.version) 
+print("TEST wsgi.py")
+
+for i in sys.path:
+  print(">>", i)
 
 import os
+
+print("XXXXXXXXXXXXXXX", os.getcwd())
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'svis.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'svis-test.settings')
+os.environ["DJANGO_SETTINGS_MODULE"] = "svis.settings"
+print("XXXXXXXXXXXXXXX 111", os.getcwd())
 application = get_wsgi_application()
 print("WSGI DJANGO running")
